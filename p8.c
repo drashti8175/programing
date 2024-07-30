6b1db2ff3e6f8415d755537fe22998bb3f7a948a
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 12:18:44 +0530

    Add files via upload

---
+++ p8.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void main()
+{
+    int burger=150,pizza=200,pasta=120,frenchfries=80,sandwich=100,total;
+    printf("menu:\n burger 150rs,\n pizza 200rs,\n pasta 120rs,\n frenchfries 80rs,\n sandwich 100rs");
+ char i=(1,2,3,4,5);
+    scanf("%d",&i);
+    switch(i)
+     {
+     case 1:
+        printf("this dish is burger order %d",1);
+        total+=burger;
+        break;
+
+             case 2:
+        printf("this dish is pizza order %d",2);
+        total+=pizza;
+        break;
+
+             case 3:
+        printf("this dish is pasta order %d",3);
+        total+=pasta;
+        break;
+
+             case 4:
+        printf("this dish is frenchfries order %d",4);
+        total+=frenchfries;
+        break;
+
+             case 5:
+        printf("this dish is sandwich order %d",5);
+        total+=sandwich;
+        break;
+
+             case 6:
+                printf("total is %d",total);
+
+                break;
+
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+}
