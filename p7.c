ff0529e369009bb8c5992bea4849b893dfbbb572
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 17:08:48 +0530

    Add files via upload

---
+++ p7.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+long ramanbangalow=12000000,plotvalued=6000000,carvalue=3000000,sumanappartmentvalued=11000000,hotelvalued=8000000,carvalued=8000000,ramantotalwealth,sumantotalwealth,totalwealth;
+
+ramantotalwealth=ramanbangalow+plotvalued+carvalue;
+sumantotalwealth=sumanappartmentvalued+hotelvalued+carvalued;
+printf("\n%ld",ramantotalwealth);
+printf("\n%ld",sumantotalwealth);
+
+
+
+if(ramantotalwealth>sumantotalwealth)
+{
+    printf("raman total wealth is greater total wealth by %d\n",sumantotalwealth);
+
+}else
+
+
+if(sumantotalwealth>ramantotalwealth)
+{
+    printf("suman total wealth is greater total wealth by %d\n",ramantotalwealth);
+}else
+{
+
+
+
+    printf("patel drashti 24tceqtj");
+}
+return 0;
+}
+
+
+
+
