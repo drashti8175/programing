f1d11b1ee275b9c8ccacf34ad21bd6ef632374a9
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 17:07:43 +0530

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a=5,b=7,add,sub,mul,div;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+printf("\n%d,addition",add);
+printf("\n%d,subtraction",sub);
+printf("\n%d,multiplication",mul);
+printf("\n%d,division",div);
+printf("patel drashti,24tceqtj");
+}
