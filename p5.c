b82d7745bfb421fe223f750022c83b5ca28ded60
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 17:05:50 +0530

    Add files via upload

---
+++ p5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float celsious=74,fahrenheit;
+printf("tempreture in celsious\n");
+fahrenheit=(celsious*9/5)+32;
+printf("convert into fahrenheit\n%f",fahrenheit);
+printf("patel drashti 24tceqtj");
+return 0;
+}
