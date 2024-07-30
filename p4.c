bc63f408d9da696ead260f66699718c473fc5454
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 17:00:22 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+
+{
+float population=1441981744,women,men,lwomen,lmen,illiteratewomen,illiteratemen;
+lmen=0.809;
+lwomen=0.628;
+women=population*0.484;
+men=population-women;
+illiteratewomen=women-lmen;
+illiteratemen=men-lwomen;
+printf("desire output for home ministry");
+printf("illiteratewomen=%d",illiteratewomen);
+printf("illiteratemen=%d", illiteratemen);
+printf("patel drashti,24tceqtj");
+
+
+
+
+}
