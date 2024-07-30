d7be9f924cbbbc40c722daf6a773912eb5ae85e3
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 17:01:58 +0530

    Add files via upload

---
+++ pr2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int length=5,breadth=9;
+int area=length*breadth,perameter=length+breadth;
+printf("/n%d",area);
+printf("/n%d",perameter);
+printf("patel drashti 24tceqtj");
+}
