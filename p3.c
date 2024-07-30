2fb17a383962074efe68e1e38e2673f0c60eadd1
patel drashti <drashtipatelce14@gmail.com>
2024-07-30 17:02:23 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+long principleamount=50000;
+float rate=0.055,simpleinterest;
+int period=3;
+simpleinterest=principleamount*rate*period;
+printf("\n%f",simpleinterest);
+printf("patel drashti 24tceqtj");
+return 0;
+}
