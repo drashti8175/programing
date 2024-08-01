433e024cff41a967d2c048a8581380903ef17335
patel drashti <drashtipatelce14@gmail.com>
2024-08-01 17:57:28 +0530

    Add files via upload

---
+++ p12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+  int x,total=0;
+  for(x=1;x<=100;x++)
+  {
+
+    if(x%2==0)
+    {
+        total=total+200;
+
+    }
+
+
+
+  }
+printf("enter the total cost:%d\n",total);
+    printf("patel drashti 24tceqtj");
+}
