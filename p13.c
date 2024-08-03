ed3ea5bc82d87d4b28b2a09347ebf00e34020e0a
patel drashti <drashtipatelce14@gmail.com>
2024-08-03 11:42:22 +0530

    Add files via upload

---
+++ p13.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+  int i=10,j=5;
+  printf("enter the value of verticleline");
+  scanf("%d:",&i);
+  printf("enter the value of horizontelline");
+  scanf("%d:",&j);
+  for(i=1;i<=10;i++)
+  {
+      for(j=1;j<=5;j++)
+      {
+          printf("%5d",i*j);
+      }
+
+
+      printf("\n");
+
+
+  }
+
+
+
+printf("patel drashti 24tceqtj");
+
+
+
+
+}
