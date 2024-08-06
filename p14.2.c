7fbfeb4c2eddbd2290d394edbb1e667b3297c1bc
patel drashti <drashtipatelce14@gmail.com>
2024-08-06 12:19:39 +0530

    Add files via upload

---
+++ p14.2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,a,k;
+
+    printf("enter the size of verticleline \n");
+    scanf("%d",&a);
+
+
+    for(i=1;i<=a;i++)
+    {
+
+
+            for(k=a;k>=i;k--)
+            {
+                printf(" ");
+            }
+
+            for(j=1;j<=i;j++)
+
+            {
+                printf("%d",j);
+            }
+
+
+        printf("\n");
+
+
+    }
+
+printf("patel drashti 24tceqtj");
+}
+
+
+
+
+
+
+
+
+
+
